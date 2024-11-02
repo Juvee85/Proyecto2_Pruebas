1642b42a7457b7eaa0ac06903e5dbe2de06018ba
SaulNeri12 <saul.neri247498@potros.itson.edu.mx>
2024-11-02 00:19:16 -0600

    Instalacion Mockito
    Se agrego la dependencia de Mockito

--- Persistencia/target/generated-sources/annotations/entidades/AutomovilEntidad_.java
+++ Persistencia/target/generated-sources/annotations/entidades/AutomovilEntidad_.java
@@ -4,7 +4,7 @@ import javax.annotation.processing.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-04-09T22:23:48", comments="EclipseLink-2.7.12.v20230209-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-11-01T23:54:53", comments="EclipseLink-2.7.12.v20230209-rNA")
 @StaticMetamodel(AutomovilEntidad.class)
 public class AutomovilEntidad_ extends VehiculoEntidad_ {
 
--- Persistencia/target/site/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"conexion"},{"l":"daos"},{"l":"entidades"},{"l":"excepciones"}];updateSearchResults();
